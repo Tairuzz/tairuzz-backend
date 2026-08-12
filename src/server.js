d5674f875f8f7ae72eda129c14a0414b882cacb5
Tairuzz <sadiq@tairuzz.co.uk>
2026-08-12 12:40:37 +0100

    Use environment variable for server port

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/api/embed-config", embedConfigRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
